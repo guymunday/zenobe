94161b876b61f08fb8bc70be84c04d1beba12a87
Guy <guymunday@gmail.com>
2020-11-23 16:27:25 +0000

    staging

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
-require("dotenv").config({
-  path: `.env.GATSBY_CONCURRENT_DOWNLOAD`,
-})
+// require("dotenv").config({
+//   path: `.env.GATSBY_CONCURRENT_DOWNLOAD`,
+// })
 
-// require .env.development or .env.production
-require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
-})
+// // require .env.development or .env.production
+// require("dotenv").config({
+//   path: `.env.${process.env.NODE_ENV}`,
+// })
 
 module.exports = {
   plugins: [
@@ -37,7 +37,7 @@ module.exports = {
     {
       resolve: `gatsby-source-wordpress-experimental`,
       options: {
-        url: process.env.WPGRAPHQL_URL || `http://zenobe.local/graphql`,
+        url: `http://34.249.174.105/graphql`,
         verbose: true,
         develop: {
           hardCacheMediaFiles: true,
@@ -46,24 +46,6 @@ module.exports = {
           graphql: {
             writeQueriesToDisk: true,
           },
-        },
-        type: {
-          Post: {
-            limit:
-              process.env.NODE_ENV === `development`
-                ? // Lets just pull 50 posts in development to make it easy on ourselves.
-                  50
-                : // and we don't actually need more than 5000 in production for this particular site
-                  5000,
-          },
-        },
-      },
-    },
-    {
-      resolve: "gatsby-plugin-react-svg",
-      options: {
-        rule: {
-          include: /\.inline\.svg$/, // See below to configure properly
         },
       },
     },
